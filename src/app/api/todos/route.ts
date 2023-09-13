1f39f3cc8947c5ee3e8253fe405c42f05616c7e5
kriztiank <kriztiank@gmail.com>
2023-09-13 10:24:07 +0200

    DELETE route handler

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,10 +3,30 @@ import { NextResponse } from 'next/server'
 // http://localhost:3000/api/todos
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
 
+const API_KEY: string = process.env.DATA_API_KEY as string
+
+// GET route handler
 export async function GET() {
     const res = await fetch(DATA_SOURCE_URL)
 
     const todos: Todo[] = await res.json()
 
     return NextResponse.json(todos)
+}
+
+// DELETE route handler
+export async function DELETE(request: Request) {
+const { id }: Partial<Todo> = await request.json()
+
+if (!id) return NextResponse.json({ "message": "Todo id required" })
+
+await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+        'API-Key': API_KEY
+    }
+})
+
+return NextResponse.json({ "message": `Todo ${id} deleted` })
 }

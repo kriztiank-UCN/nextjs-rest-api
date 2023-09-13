b35cfb357256064cce60b7f0d0daebbbc175c2e3
kriztiank <kriztiank@gmail.com>
2023-09-13 12:21:44 +0200

    Dynamic API route

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+
+// http://localhost:3000/api/todos
+const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+// GET route handler
+export async function GET(request: Request) {
+  const id = request.url.slice(request.url.lastIndexOf('/') + 1)
+
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`)
+
+  const todo: Todo = await res.json()
+
+  if (!todo.id) return NextResponse.json({ message: 'Todo not found' })
+
+  return NextResponse.json(todo)
+}
